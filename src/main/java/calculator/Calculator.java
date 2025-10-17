d1b83ee9ab7d13fe980a0f9a983754b6575cd655
ddangme <ssilver121@naver.com>
2025-10-17 09:08:21 +0900

    feat: 입력 기능 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Calculator {
+
+    public void main() {
+        String input = input();
+    }
+
+    private String input() {
+        return Console.readLine();
+    }
+}
